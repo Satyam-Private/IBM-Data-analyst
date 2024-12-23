4a68d0201b3504948dcc2bbcfd300d47c7c3447b
Satyam-Private <satyampatil1252@gmail.com>
2024-12-23 22:09:33 +0530

    getting started with python

---
+++ Python_for_DS/intro.py
@@ -0,0 +1,15 @@
+print("first python file")
+
+
+# to check the type of variable in python we use type() function
+
+a = 1
+b = 2.0
+c = "hello"
+d = True 
+#above are the basic data types in python
+#to check the type of variable 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
