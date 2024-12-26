3f80aa9f865e9e884f40b7481c0a714d18398bda
Satyam-Private <satyampatil1252@gmail.com>
2024-12-26 23:46:17 +0530

    Added comments

--- Data_visualization_with_python/Matplot.py
+++ Data_visualization_with_python/Matplot.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
+
+#sample random data
 x = [1, 2, 3, 4, 5]
 y = [10 , 20 , 30 , 40 , 50]
 
-
+#convert to dataframe
 dataframe = pd.DataFrame({'x':x, 'y':y})
 print(dataframe)
+
+#plotting with matplotlib
 # plt.plot(x,y)
 # plt.show()
 
+#plotting with pandas
 dataframe.plot(x='x', y='y', kind='line')
